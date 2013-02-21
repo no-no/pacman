6d46aa831c44d45d04fc33401a76e3baa1312709
Yuichi Okuno <y-okuno@biglobe.co.jp>
2013-02-21 23:00:31 +0900

    リファクタリング Signed-off-by: Yuichi Okuno <y-okuno@biglobe.co.jp>

--- src/com/example/pacman/GameController.java
+++ src/com/example/pacman/GameController.java
@@ -9,40 +9,30 @@ import android.view.View;
 
 public class GameController extends View {
 
-	public int x = 100, y = 100;
-	public int xx, yy;
-	private final String tag = "Android";
+	public int touchPositionX;
+	public int touchPositionY;
+
+	Pacman pacman;
 
 	public GameController(Context context) {
 		super(context);
+		pacman = new Pacman(context);
 	}
 
 	protected void onDraw(Canvas canvas) {
 		Paint paint = new Paint();
-		paint.setColor(Color.BLACK);
-		paint.setAntiAlias(true);
-		canvas.drawCircle(x, y, 50, paint);
-		String log = String.valueOf(xx) + ", " + String.valueOf(yy);
-		canvas.drawText(log, 10, 10, paint);
+		String touchPosition = "タッチしたポジション" + String.valueOf(this.touchPositionX) + ", "
+				+ String.valueOf(this.touchPositionY);
+		canvas.drawText(touchPosition, 10, 10, paint);
 	}
 
-	public boolean onTouchEvent(MotionEvent e) {
-
-		xx = (int) e.getX();
-		yy = (int) e.getY();
-		if (((int) xx >= x - 100) && ((int) xx <= x + 100)
-				&& ((int) yy >= y - 100) && ((int) yy <= y + 100)) {
-			switch (e.getAction()) {
-			case MotionEvent.ACTION_MOVE:
-				x = (int) e.getX();
-				y = (int) e.getY();
-				break;
-			default:
-				break;
-			}
-		}
-		invalidate();
-		return true;
+	/**
+	 * タッチしたポジションに更新する
+	 */
+	public void disp(MotionEvent event) {
+		this.touchPositionX = (int) event.getX();
+		this.touchPositionY = (int) event.getY();
+		this.invalidate();
 	}
 
 }
--- src/com/example/pacman/MainActivity.java
+++ src/com/example/pacman/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.pacman;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
+	private RelativeLayout viewGroup;
+	private Pacman pacman;
+	private GameController gameController;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		GameController pacman = new GameController(this);
-		setContentView(pacman);
+		viewGroup = new RelativeLayout(this);
+		gameController = new GameController(this);
+		viewGroup.addView(gameController);
+		pacman = new Pacman(this);
+		viewGroup.addView(pacman);
+
+		setContentView(viewGroup);
+		// setContentView(pacman);
 		// setContentView(R.layout.activity_main);
 	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		gameController.disp(e);
+		switch (e.getAction()) {
+		case MotionEvent.ACTION_MOVE:
+			pacman.move(e);
+			break;
+		default:
+			break;
+		}
+		return true;
+	}
+
 }
